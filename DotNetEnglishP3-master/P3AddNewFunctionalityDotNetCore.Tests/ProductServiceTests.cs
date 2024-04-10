7b34210f6a7d205b56fe4da52af76e5446c49431
Bob Léo <linksoulcalibur@msn.com>
2024-04-10 20:13:21 +0200

    Ajout d'un nugget Microsoft.FrameCore.InMemory Ajout des test d'intégrations: SaveProductTest_ShouldBeContainsNewProduct AddNewProductInTheCart DeleteProductTest_ShouldBeDeleteProductById

--- DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore.Tests/ProductServiceTests.cs
+++ DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore.Tests/ProductServiceTests.cs
@@ -6,6 +6,10 @@ using P3AddNewFunctionalityDotNetCore.Models.ViewModels;
 using P3AddNewFunctionalityDotNetCore.Models;
 using System.Collections.Generic;
 using Xunit;
+using Microsoft.EntityFrameworkCore;
+using P3AddNewFunctionalityDotNetCore.Data;
+using P3AddNewFunctionalityDotNetCore.Models.Entities;
+using System.Linq;
 
 namespace P3AddNewFunctionalityDotNetCore.Tests
 {
@@ -206,6 +210,170 @@ namespace P3AddNewFunctionalityDotNetCore.Tests
             Assert.NotEmpty(resultQuantityIsNotPositive);
             Assert.Contains("Stock is not positive error message", resultQuantityIsNotPositive);
 
+        }
+        /// <summary>
+        /// Verify if the product is adding in the inventory
+        /// </summary>
+        [Fact]
+        public void SaveProductTest_ShouldBeContainsNewProduct()
+        {
+            // Arrange
+            Mock<ICart> mockCart = new Mock<ICart>();
+            Mock<IOrderRepository> mockOrderRepository = new Mock<IOrderRepository>();
+            Mock<IStringLocalizer<ProductService>> mockLocalizer = new Mock<IStringLocalizer<ProductService>>();
+
+            // Create options for in-memory database
+            var options = new DbContextOptionsBuilder<P3Referential>()
+               .UseInMemoryDatabase(databaseName: "data name")
+               .Options;
+
+            // Create real context for ProductRepository
+            P3Referential context = new P3Referential(options, null);
+            {
+                // Seed the in-memory database with some initial products
+                context.Product.AddRange(new List<Product>
+                {
+                    new Product { Id = 1, Name = "Product 1", Price = 10.99, Quantity = 50 },
+                    new Product { Id = 2, Name = "Product 2", Price = 24.99, Quantity = 30 },
+                    new Product { Id = 3, Name = "Product 3", Price = 54.99, Quantity = 20 }
+                });
+                context.SaveChanges();
+
+                // Create real ProductRepository using the context
+                ProductRepository productRepository = new ProductRepository(context);
+
+                ProductService productService = new ProductService(mockCart.Object, productRepository, mockOrderRepository.Object, mockLocalizer.Object);
+
+                ProductViewModel newProductViewModel = new ProductViewModel
+                {
+                    Name = "New Product",
+                    Price = 19.99.ToString(),
+                    Stock = 50.ToString(),
+                    Description = "This is a new product",
+                    Details = "Some details about the new product"
+                };
+
+                // Act
+                productService.SaveProduct(newProductViewModel);
+
+                // Instance the list after the add the product
+                List<ProductViewModel> updatedProducts = productService.GetAllProductsViewModel();
+
+                // Assert
+                // Verify if the product is in the inventory
+                Assert.Contains(updatedProducts, p => p.Name == "New Product");
+
+                // Check if the originals products is the inventory after the update
+                Assert.Contains(updatedProducts, p => p.Name == "Product 1");
+                Assert.Contains(updatedProducts, p => p.Name == "Product 2");
+                Assert.Contains(updatedProducts, p => p.Name == "Product 3");
+            }
+        }
+
+
+        [Fact]
+        public void AddNewProductInTheCart()
+        {
+            // Arrange
+            Cart cart = new Cart();
+            Mock<IOrderRepository> mockOrderRepository = new Mock<IOrderRepository>();
+            Mock<IStringLocalizer<ProductService>> mockLocalizer = new Mock<IStringLocalizer<ProductService>>();
+
+            // Get options in the in-memory database "data name" instanced in SaveProductTest_ShouldBeContainsNewProduct()
+            var options = new DbContextOptionsBuilder<P3Referential>()
+               .UseInMemoryDatabase(databaseName: "data name")
+               .Options;
+
+            // Create real context for ProductRepository
+            P3Referential context = new P3Referential(options, null);
+            {
+                // Create real ProductRepository using the context
+                ProductRepository productRepository = new ProductRepository(context);
+
+                ProductService productService = new ProductService(cart, productRepository, mockOrderRepository.Object, mockLocalizer.Object);
+
+                // Instance the list 
+
+                List<Product> Products = productService.GetAllProducts();
+
+
+
+                // Act
+                cart.AddItem(Products.First(p => p.Name == "New Product"), 1);
+                cart.AddItem(Products.First(p => p.Name == "Product 1"), 1);
+
+
+                // Assert
+                // Verify if the product is in the inventory 
+                Assert.Contains(Products, p => p.Name == "New Product");
+
+                // Verify if the products are in the cart
+                Assert.NotEmpty(cart.Lines);
+                Assert.Equal(2, cart.Lines.Count());
+
+                //If the assert passed then item removed was "New Product"
+                cart.RemoveLine(Products.First(p => p.Name == "New Product"));
+                Assert.Equal(1, cart.Lines.Count());
+            }
+        }
+
+        /// <summary>
+        /// Verify if the product is adding in the inventory
+        /// </summary>
+        [Fact]
+        public void DeleteProductTest_ShouldBeDeleteProductById()
+        {
+            // Arrange
+            Cart cart = new Cart();
+            Mock<IOrderRepository> mockOrderRepository = new Mock<IOrderRepository>();
+            Mock<IStringLocalizer<ProductService>> mockLocalizer = new Mock<IStringLocalizer<ProductService>>();
+
+            // Get options in the in-memory database "data name 2"
+            var options = new DbContextOptionsBuilder<P3Referential>()
+               .UseInMemoryDatabase(databaseName: "data name 2")
+               .Options;
+
+            // Create real context for ProductRepository
+            P3Referential context = new P3Referential(options, null);
+            {
+                // Seed the in-memory database with some initial products
+                context.Product.AddRange(new List<Product>
+                {
+                    new Product { Id = 1, Name = "Product 1", Price = 10.99, Quantity = 50 },
+                    new Product { Id = 2, Name = "Product 2", Price = 24.99, Quantity = 30 },
+                    new Product { Id = 3, Name = "Product 3", Price = 54.99, Quantity = 20 }
+                });
+                context.SaveChanges();
+                // Create real ProductRepository using the context
+                ProductRepository productRepository = new ProductRepository(context);
+
+                ProductService productService = new ProductService(cart, productRepository, mockOrderRepository.Object, mockLocalizer.Object);
+
+                // Instance the list 
+                List<Product> Products = productService.GetAllProducts();
+
+                // Verify if the product is in the inventory before delete
+                Assert.Contains(Products, p => p.Name == "Product 2");
+                cart.AddItem(Products.First(p => p.Name == "Product 2"), 1);
+                Assert.NotEmpty(cart.Lines);
+
+                // Act
+                productService.DeleteProduct(2);
+
+                // Instance the list after the delete
+                Products = productService.GetAllProducts();
+
+
+                // Assert
+                // Verify if the product is in the inventory
+                Assert.Empty(cart.Lines);
+                Assert.Equal(2, Products.Count);
+
+                // Check if the originals products is the inventory after the update
+                Assert.Contains(Products, p => p.Name == "Product 1");
+                Assert.Contains(Products, p => p.Name == "Product 3");
+
+            }
         }
     }
 }
